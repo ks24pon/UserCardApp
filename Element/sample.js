e0bf22f2a8c85fc52649ffab569ec3333a7715a2
ks24pon <pom123re0624@gmail.com>
2025-10-22 09:27:20 +0900

    innerHTML

---
+++ Element/sample.js
@@ -0,0 +1,19 @@
+let ele1 = document.getElementById("apple");
+let appleString = " the world's most advanced operating system";
+ele1.innerHTML = "Apple offers" + appleString;
+console.log(ele1);
+
+let ele2 = document.getElementById("netflix");
+let netflixString = " entertainment streaming services";
+ele2.innerHTML = "Netflix offers" + netflixString;
+console.log(ele2);
+
+let ele3 = document.getElementById("facebook");
+let facebookString = " social platform where people can hang out";
+ele3.innerHTML = "Facebook offers" + facebookString;
+console.log(ele3);
+
+let ele4 = document.getElementById("tesla");
+let teslaString = " electric vehicles with clean energy";
+ele4.innerHTML = "Tesla offers" + teslaString;
+console.log(ele4);
